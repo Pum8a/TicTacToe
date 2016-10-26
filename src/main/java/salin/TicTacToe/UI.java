7057e442bd243bfa6a410dd084dc8f438e54cc94
Sigurður unnar Hauksson <Sigurur unnar Hauksson>
2016-10-26 19:56:55 +0000

    menu function for UI

--- src/main/java/salin/TicTacToe/UI.java
+++ src/main/java/salin/TicTacToe/UI.java
@@ -23,11 +23,28 @@ public class UI {
 		
 		
 		System.out.println("1 - Play ");
-		System.out.println("2 - Credit");
-		System.out.println("3 - Exit");
+		System.out.println("2 - Info");
+		System.out.println("3 - Credit");
+		System.out.println("Every thing else quits.")
 
 		Scanner scan = new Scanner(System.in);
 		int x = scan.nextInt();
+
+		switch (x){
+			case 1:
+				   //Fall sem spilar leikinn úr game klasanum
+			       break;
+			case 2: 
+				   //Info um leikinn. Hvernig hann er spilaður og þess háttar
+			       break;
+			case 3:
+				   //Credit 
+			       break;
+			default :
+					break;
+					
+		}
+
 
 		System.out.println(x);
 	}
