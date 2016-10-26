ebe06b8c95c0b81f1f06ca4098bbd28c32f7c048
Sigurður unnar Hauksson <Sigurur unnar Hauksson>
2016-10-26 18:06:22 +0000

    added menu function to UI

--- src/main/java/salin/TicTacToe/UI.java
+++ src/main/java/salin/TicTacToe/UI.java
@@ -1,9 +1,15 @@
 package salin.TicTacToe;
 
+import java.util.Scanner;
+
 public class UI {
 
 	public static void main(String args[]) {
+		Map map = new Map();
+		Player player = new Player();
 		header();
+		menu();
+
 	}
 
 	private static void header() {
@@ -12,5 +18,17 @@ public class UI {
 		System.out.println("*             Tic Tac Toe            *");
 		System.out.println("*                                    *");
 		System.out.println("**************************************");
+	}
+	public static void menu() {
+		
+		
+		System.out.println("1 - Play ");
+		System.out.println("2 - Credit");
+		System.out.println("3 - Exit");
+
+		Scanner scan = new Scanner(System.in);
+		int x = scan.nextInt();
+
+		System.out.println(x);
 	}
 }
