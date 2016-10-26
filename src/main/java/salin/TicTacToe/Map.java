953874824b0a9affe0aa810e968926bf672c04dd
KalliKall <olafurut15@ru.is>
2016-10-26 17:19:05 +0000

    update map test success

--- src/main/java/salin/TicTacToe/Map.java
+++ src/main/java/salin/TicTacToe/Map.java
@@ -1,5 +1,7 @@
 package salin.TicTacToe;
 
+import static java.lang.System.out;
+
 public class Map {
 	private int size;
 	private String[] map;
@@ -7,17 +9,22 @@ public class Map {
 	public Map() {
 		int size = 9;
 		map = new String[size];
-		for(int i=1; i<size; i++) {
-			map[i] = Integer.toString(i);
+		for(int i=0; i<size; i++) {
+			map[i] = Integer.toString(i+1);
 		}
 		
 	}
 	
 	public String getCurrent(int key) {
-		return map[key];
+		return map[key-1];
 	}
 	
 	public int getSize() {
 		return size;
 	}
+	
+	public void updateMap(int key, String Player) {
+		map[key-1] = Player;
+	}
+	
 }
