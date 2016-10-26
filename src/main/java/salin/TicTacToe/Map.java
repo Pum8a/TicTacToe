8567862ee86fb99fdb600f1fb0738111cc31f074
KalliKall <olafurut15@ru.is>
2016-10-26 16:41:53 +0000

    Test on create map succes

--- src/main/java/salin/TicTacToe/Map.java
+++ src/main/java/salin/TicTacToe/Map.java
@@ -1,1 +1,24 @@
 package salin.TicTacToe;
+
+public class Map {
+	private int size;
+	private String[] map;
+	
+	public Map() {
+		int size = 9;
+		map = new String[size];
+		for(int i=1; i<size; i++) {
+			map[i] = Integer.toString(i);
+		}
+		
+	}
+	
+	public String getCurrent(int key) {
+		return map[key];
+	}
+	
+	public int getSize() {
+		return size;
+	}
+	
+}
--- src/test/java/salin/TicTacToe/MapTest.java
+++ src/test/java/salin/TicTacToe/MapTest.java
@@ -1,7 +1,14 @@
 package salin.TicTacToe;
 
 import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
 public class MapTest {
-	
+	@Test
+	public void MapCreateTest() {
+		Map map = new Map();
+		for(int i=1; i<=map.getSize(); i++) {
+			assertEquals(Integer.toString(i), map.getCurrent(i));
+		}
+	}
 }
